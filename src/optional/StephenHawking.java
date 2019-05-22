2d600934d0663e63a183effb65a302983dab8349
makotosvetlapali <donpedjas@gmail.com>
2019-05-22 10:24:24 +0200

    First commit

--- src/optional/StephenHawking.java
+++ src/optional/StephenHawking.java
@@ -1,16 +1,22 @@
 package optional;
 
+import javax.swing.JOptionPane;
+
 import voce.SpeechSynthesizer;
 
 public class StephenHawking {
 
-	// 1. make a main method and put steps 2, 3 & 4 inside it
-	// 2. ask the user for a sentence
-	// 3. call the speak method below and send it the sentence
-	// 4. repeat steps 2 and 3 a lot of times
-
+	public static void main(String[] args) {
+		
+		for(int i = 0; i < 100; i++) {
+			String sentence = JOptionPane.showInputDialog("Type your sentence:");
+			if (sentence == null) System.exit(0);
+			speak(sentence);
+		}
+	}
 	
-	/* Donâ€™t change thisâ€¦. */
+	
+	/* Don’t change this…. */
 	static void speak(String words) {
 		SpeechSynthesizer speaker = new SpeechSynthesizer("speaker");
 		speaker.synthesize(words);
